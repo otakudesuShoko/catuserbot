933644ebbad1ed34be6d17c95f07669920149adb
otakudesuShoko <76880423+otakudesuShoko@users.noreply.github.com>
2021-01-13 22:47:50 +0800

    Update covid.py

--- userbot/plugins/covid.py
+++ userbot/plugins/covid.py
@@ -21,12 +21,12 @@ async def corona(event):
         hmm1 = country_data["confirmed"] + country_data["new_cases"]
         hmm2 = country_data["deaths"] + country_data["new_deaths"]
         data = ""
-        data += f"\n⚠️ Confirmed   : <code>{hmm1}</code>"
-        data += f"\n😔 Active           : <code>{country_data['active']}</code>"
-        data += f"\n⚰️ Deaths         : <code>{hmm2}</code>"
-        data += f"\n🤕 Critical          : <code>{country_data['critical']}</code>"
-        data += f"\n😊 Recovered   : <code>{country_data['recovered']}</code>"
-        data += f"\n💉 Total tests    : <code>{country_data['total_tests']}</code>"
+        data += f"\n⚠️ Dikonfirmasi   : <code>{hmm1}</code>"
+        data += f"\n😔 Kasus positif  : <code>{country_data['active']}</code>"
+        data += f"\n⚰️ Kasus kematian : <code>{hmm2}</code>"
+        data += f"\n🤕 Sedang Dirawat : <code>{country_data['critical']}</code>"
+        data += f"\n😊 Sembuh          : <code>{country_data['recovered']}</code>"
+        data += f"\n💉 Total Test      : <code>{country_data['total_tests']}</code>"
         data += f"\n🥺 New Cases   : <code>{country_data['new_cases']}</code>"
         data += f"\n😟 New Deaths : <code>{country_data['new_deaths']}</code>"
         await catevent.edit(
