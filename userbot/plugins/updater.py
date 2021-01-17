d8baaf3be74ad4265db79c6e44784274c88a4955
otakudesuShoko <76880423+otakudesuShoko@users.noreply.github.com>
2021-01-17 17:13:02 +0800

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -37,7 +37,7 @@ async def gen_chlog(repo, diff):
 
 async def print_changelogs(event, ac_br, changelog):
     changelog_str = (
-        f"**New UPDATE available for [{ac_br}]:\n\nCHANGELOG:**\n`{changelog}`"
+        f"**Update terbaru untuk [{ac_br}]:\n\nHal Baru:**\n`{changelog}`"
     )
     if len(changelog_str) > 4096:
         await event.edit("`Changelog is too big, view the file to see it.`")
