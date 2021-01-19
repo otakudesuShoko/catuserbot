75b52eff962811107b210c2bce95cdde643bb935
otakudesuShoko <76880423+otakudesuShoko@users.noreply.github.com>
2021-01-19 13:21:54 +0800

    Update quotly.py

--- userbot/plugins/quotly.py
+++ userbot/plugins/quotly.py
@@ -19,15 +19,15 @@ async def stickerchat(catquotes):
     reply = await catquotes.get_reply_message()
     if not reply:
         await edit_or_reply(
-            catquotes, "`I cant quote the message . reply to a message`"
+            catquotes, "`Tindakan anda salah . Balas di pesan`"
         )
         return
     fetchmsg = reply.message
     repliedreply = None
     if reply.media and reply.media.document.mime_type in ("mp4"):
-        await edit_or_reply(catquotes, "`this format is not supported now`")
+        await edit_or_reply(catquotes, "`Mohon maaf format media tidak mendukung`")
         return
-    catevent = await edit_or_reply(catquotes, "`Making quote...`")
+    catevent = await edit_or_reply(catquotes, "`Membuat stiker...`")
     user = (
         await event.client.get_entity(reply.forward.sender)
         if reply.fwd_from
@@ -52,15 +52,15 @@ async def stickerchat(catquotes):
     reply = await catquotes.get_reply_message()
     if not reply:
         await edit_or_reply(
-            catquotes, "`I cant quote the message . reply to a message`"
+            catquotes, "`Tindakan anda salah, Balas di pesan`"
         )
         return
     fetchmsg = reply.message
     repliedreply = await reply.get_reply_message()
     if reply.media and reply.media.document.mime_type in ("mp4"):
-        await edit_or_reply(catquotes, "`this format is not supported now`")
+        await edit_or_reply(catquotes, "`Mohon maaf format media tidak mendukung`")
         return
-    catevent = await edit_or_reply(catquotes, "`Making quote...`")
+    catevent = await edit_or_reply(catquotes, "`Membuat stiker...`")
     user = (
         await event.client.get_entity(reply.forward.sender)
         if reply.fwd_from
@@ -83,14 +83,14 @@ async def _(event):
     if event.fwd_from:
         return
     if not event.reply_to_msg_id:
-        await edit_or_reply(event, "```Reply to any user message.```")
+        await edit_or_reply(event, "```Balas pesan si anak haram.```")
         return
     reply_message = await event.get_reply_message()
     if not reply_message.text:
-        await edit_or_reply(event, "```Reply to text message```")
+        await edit_or_reply(event, "```Balas pesan si anak haram```")
         return
     chat = "@QuotLyBot"
-    catevent = await edit_or_reply(event, "```Making a Quote```")
+    catevent = await edit_or_reply(event, "```Membuat stiker```")
     async with event.client.conversation(chat) as conv:
         try:
             response = conv.wait_event(
@@ -99,12 +99,12 @@ async def _(event):
             await event.client.forward_messages(chat, reply_message)
             response = await response
         except YouBlockedUserError:
-            await catevent.edit("```Please unblock me (@QuotLyBot) u Nigga```")
+            await catevent.edit("```Buka Blok (@QuotLyBot) Asw```")
             return
         await event.client.send_read_acknowledge(conv.chat_id)
         if response.text.startswith("Hi!"):
             await catevent.edit(
-                "```Can you kindly disable your forward privacy settings for good?```"
+                "```Mohon menonaktifkan privasi anda```"
             )
         else:
             await catevent.delete()
